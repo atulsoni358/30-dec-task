eb7bf5d7b3d013145be456f8cf195b5998024285
atulsoni358 <atul.soni358@gmail.com>
2016-12-30 12:24:18 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,10 @@
+var a = 5;
+console.log(a);
+function add () {
+  var b = 6;
+  console.log(b);
+  console.log(a);
+}
+add();
+console.log(a);
+console.log(b);
---
+++ task2.js
@@ -0,0 +1,14 @@
+var a=5;
+console.log(a);
+
+function add () {
+  console.log(a);
+  console.log(c);
+  var a = 3;
+  var b = 6;
+ console.log(b);
+ console.log(a);
+}
+add();
+console.log(a);
+console.log(b);
